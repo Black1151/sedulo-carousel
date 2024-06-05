1ce3ad848d585b5d4a5c658ed96b4dbcc2d8e3ec
Daniel Black <danblack9988@gmail.com>
2024-06-05 15:29:19 +0100

    fix HMR not working

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.watchOptions = {
+        ignored: /node_modules/,
+        poll: 1000,
+      }
+    }
+    return config
+  },
+}
 
-export default nextConfig;
+export default nextConfig
